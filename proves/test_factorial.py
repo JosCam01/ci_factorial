68b0a0b31c9dd3a9da0a4c07646dcf151e48f74f
Jose Camarena <josecamarenag01@gmail.com>
2024-02-16 22:48:58 +0100

    Update test_factorial.py

--- proves/test_factorial.py
+++ proves/test_factorial.py
@@ -1,20 +1,19 @@
-import pytest
-import src.funcio_factorial as funcio_factorial
+import pytest, ci_factorial.src.funcio_factorial as factorial
 
 
 def test_factorial_de_1():
-    assert funcio_factorial.factorial(1) == 1
+    assert factorial.factorial(1) == 1
 
 
 def test_factorial_type_error():
     with pytest.raises(TypeError):
-        funcio_factorial.factorial(2.5)
+        factorial.factorial(2.5)
 
 
 def test_factorial_value_error():
     with pytest.raises(ValueError):
-        funcio_factorial.factorial(-3)
+        factorial.factorial(-3)
 
 
 def test_factorial_de_5():
-    assert funcio_factorial.factorial(5) == 120
+    assert factorial.factorial(5) == 120
