27b97505cbd8b8dda453cfc675ed3c5ae493b535
Jose Camarena <josecamarenag01@gmail.com>
2024-02-16 22:41:09 +0100

    pylint 8.89

--- src/funcio_factorial.py
+++ src/funcio_factorial.py
@@ -1,4 +1,23 @@
+"""
+funcio_factorial module
+A simple module to calculate the factorial of a non-negative integer.
+"""
+
+
 def factorial(n):
+    """
+    Calculate the factorial of a non-negative integer.
+
+    Args:
+        n (int): The integer to calculate the factorial for.
+
+    Returns:
+        int: The factorial of the input integer.
+
+    Raises:
+        TypeError: If n is not an integer.
+        ValueError: If n is negative.
+    """
     if not isinstance(n, int):
         raise TypeError("El factorial solo puede calcularse para enteros")
     if n < 0:
